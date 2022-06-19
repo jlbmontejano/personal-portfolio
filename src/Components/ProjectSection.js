97fd55591b4d699a0043530a2f461e05a6a5fc87
unknown <jlbmontejano@gmail.com>
2022-06-18 20:50:33 -0700

    improved const naming and overall order of the file

--- src/Components/ProjectSection.js
+++ src/Components/ProjectSection.js
@@ -3,12 +3,13 @@ import IndividualProject from "./IndividualProject";
 import "./ProjectSection.css";
 
 const ProjectSection = () => {
-  const AdviceGenerator = {
-    title: "Advice Generator",
-    src: `${process.env.PUBLIC_URL}/images/advice-generator.png`,
-    description: "Small project that fetches an advice from an API.",
-    link: "https://shimmering-bunny-4c923a.netlify.app/",
-    code: "https://github.com/jlbmontejano/advice-generator-app",
+  const PokemonApi = {
+    title: "Pokemon-API",
+    src: `${process.env.PUBLIC_URL}/images/pokemon-api.png`,
+    description:
+      "Webapp that uses the pokeapi to display different pokemon information.",
+    link: "https://relaxed-frangipane-1bb52e.netlify.app/",
+    code: "https://github.com/jlbmontejano/pokemon-api",
     key: 0,
   };
   const RockPaperScissors = {
@@ -19,31 +20,32 @@ const ProjectSection = () => {
     code: "https://github.com/jlbmontejano/rock-paper-scissors",
     key: 1,
   };
-  const PokemonApi = {
-    title: "Pokemon-API",
-    src: `${process.env.PUBLIC_URL}/images/pokemon-api.png`,
-    description:
-      "Webapp that uses the pokeapi to display different pokemon information.",
-    link: "https://relaxed-frangipane-1bb52e.netlify.app/",
-    code: "https://github.com/jlbmontejano/pokemon-api",
+  const AdviceGenerator = {
+    title: "Advice Generator",
+    src: `${process.env.PUBLIC_URL}/images/advice-generator.png`,
+    description: "Small project that fetches different advices from an API.",
+    link: "https://shimmering-bunny-4c923a.netlify.app/",
+    code: "https://github.com/jlbmontejano/advice-generator-app",
     key: 2,
   };
-  const imageSource = [PokemonApi, RockPaperScissors, AdviceGenerator];
+
+  const projectList = [PokemonApi, RockPaperScissors, AdviceGenerator];
   return (
     <div>
       <h3>Projects</h3>
       <p>
-        These are some of the projects I've worked on so far. I am constantly updating and improving them.
+        These are some of the projects I've worked on so far. I am constantly
+        updating and improving them.
       </p>
       <div className="project-section">
-        {imageSource.map(image => (
+        {projectList.map(project => (
           <IndividualProject
-            title={image.title}
-            image={image.src}
-            description={image.description}
-            link={image.link}
-            code={image.code}
-            key={image.key}
+            title={project.title}
+            image={project.src}
+            description={project.description}
+            link={project.link}
+            code={project.code}
+            key={project.key}
           />
         ))}
       </div>
