d117a2b15e2ef6ecad94695799232566ff074811
unknown <jlbmontejano@gmail.com>
2022-06-22 00:24:03 -0700

    fixed css issues

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 4000);
+    setTimeout(() => setLoading(false), 3000);
   }, []);
 
   return (
