d7fbfa240a4a7e6f43d71567cde08f74cc0a6889
unknown <jlbmontejano@gmail.com>
2024-03-01 14:12:20 -0800

    Changed programming languages and tech to flex-row in XL screens

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
 			<div data-aos='fade-up' className='section-container mt-3'>
 				<h3>About Me</h3>
 				<p>As a dedicated frontend developer, I specialize in crafting innovative, efficient, and scalable solutions that drive real value. My passion for technology and problem-solving drives me to constantly seek new challenges that expand my skill set and contribute to the success of the projects I'm involved in. I am self-taught, powered by an endless fascination with logic, math, and the cool puzzles they throw my way. I'm naturally curious and pretty passionate about learning. For me, information and time are super precious, always making sure I'm learning something new or making something worthwhile. In short, I love what I do, and I'm always looking for the next cool project to get my hands on. Let's make something awesome together!</p>
-				<div className='flex flex-col gap-7'>
+				<div className='flex flex-col gap-7 xl:flex-row'>
 					<div>
 						<h4 className='font-bold'>Programming Languages: </h4>
 						<ul>
