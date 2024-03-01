9a6c35b38ee97c5af300224093b293bf4885e808
unknown <jlbmontejano@gmail.com>
2024-02-29 18:42:31 -0800

    Fixed div absolute position

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,16 +1,14 @@
 const Home = () => {
 	return (
-		<div>
-			<div id='home' className='scroll-anchor absolute' />
-			<div className='flex flex-col w-full items-center h-screen justify-center'>
-				<div className='text-left'>
-					<h1 className='italic'>Hi, my name is</h1>
-					<h1 className='font-bold'>Jorge Buenrostro</h1>
-				</div>
-				<div className='mt-2'>
-					<h5>I am a Frontend Developer </h5>
-					<h5>from Mexico</h5>
-				</div>
+		<div className='section-container gap-0 items-center h-screen justify-center'>
+			<div id='home' className='scroll-anchor absolute top-0' />
+			<div className='text-left'>
+				<h1 className='italic'>Hi, my name is</h1>
+				<h1 className='font-bold'>Jorge Buenrostro</h1>
+			</div>
+			<div className="mt-2">
+				<h5>I am a Frontend Developer </h5>
+				<h5>from Mexico</h5>
 			</div>
 		</div>
 	);
