a2e56514e4b20d10612b4578fb8f47275790cba6
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-08-20 12:23:23 -0700

    Deleted overflow-hidden class

--- src/components/IndividualWork.tsx
+++ src/components/IndividualWork.tsx
@@ -19,28 +19,51 @@ const IndividualWork = ({ work }: IndividualWorkProps) => {
 
 	return (
 		<>
-			<div className='flex flex-col w-full xl:w-1/3 '>
-				<button onClick={() => setIsOpen(true)} className='p-5 overflow-hidden'>
-					<h5 className='text-nowrap text-ellipsis whitespace-nowrap overflow-hidden'>{work.company}</h5>
-					<p className='text-sm text-nowrap text-ellipsis whitespace-nowrap overflow-hidden'>{work.position}</p>
-					<p className='text-xs text-nowrap text-ellipsis whitespace-nowrap overflow-hidden'>
+			<div className='flex flex-col w-full'>
+				<button onClick={() => setIsOpen(true)} className='p-5'>
+					<h5 className='text-nowrap text-ellipsis whitespace-nowrap'>
+						{work.company}
+					</h5>
+					<p className='text-sm text-nowrap text-ellipsis whitespace-nowrap'>
+						{work.position}
+					</p>
+					<p className='text-xs text-nowrap text-ellipsis whitespace-nowrap'>
 						{work.fromDate} - {work.toDate}
 					</p>
 				</button>
 			</div>
 			<Transition show={isOpen} as={Fragment}>
-				<Dialog onClose={() => setIsOpen(false)} initialFocus={workTitleRef} className='relative z-50'>
+				<Dialog
+					onClose={() => setIsOpen(false)}
+					initialFocus={workTitleRef}
+					className='relative z-50'>
 					{/* Backdrop */}
-					<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
+					<Transition.Child
+						as={Fragment}
+						enter='ease-out duration-300'
+						enterFrom='opacity-0'
+						enterTo='opacity-100'
+						leave='ease-in duration-200'
+						leaveFrom='opacity-100'
+						leaveTo='opacity-0'>
 						<div className='fixed inset-0 bg-black/30' aria-hidden='true' />
 					</Transition.Child>
 
 					{/* Full-screen scrollable container */}
-					<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
+					<Transition.Child
+						as={Fragment}
+						enter='ease-out duration-300'
+						enterFrom='opacity-0'
+						enterTo='opacity-100'
+						leave='ease-in duration-200'
+						leaveFrom='opacity-100'
+						leaveTo='opacity-0'>
 						<div className='fixed inset-0 p-4 overflow-y-auto'>
 							<div className='flex min-h-full items-center justify-center p-4'>
-								<Dialog.Panel className='p-5 md:p-10 bg-white rounded-xl lg:w-2/3'>
-									<Dialog.Title className='mb-5 text-center text-xl md:text-4xl' ref={workTitleRef}>
+								<Dialog.Panel className='p-5 md:p-10 bg-white rounded-xl lg:w-1/2'>
+									<Dialog.Title
+										className='mb-5 text-center text-xl md:text-4xl'
+										ref={workTitleRef}>
 										{work.company}
 									</Dialog.Title>
 									<Dialog.Description className='text-left text-sm md:text-base'>
@@ -65,7 +88,9 @@ const IndividualWork = ({ work }: IndividualWorkProps) => {
 										))}
 									</ul>
 									<div className='w-full flex justify-center '>
-										<button onClick={() => setIsOpen(false)} className='mt-5 px-6 py-2'>
+										<button
+											onClick={() => setIsOpen(false)}
+											className='mt-5 px-6 py-2'>
 											Close
 										</button>
 									</div>
