5679b3ba88e898f5a6cfd7aba1cb4c48763f3565
Jorge Laptop <jlbmontejano@gmail.com>
2025-08-26 14:22:45 -0700

    Added types to constants

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,32 @@
-export type MenuNavigation = "" | "about-me" | "work-experience" | "other";
+export type MenuOption = {
+	name: string;
+	scrollTo: string;
+};
+
+export type TechStack = {
+	title: string;
+	data: string[];
+};
+
+export type WorkExperience = {
+	number: string;
+	company: string;
+	position: string;
+	location: string;
+	fromDate: string;
+	toDate: string;
+	technologies: string;
+	tasks: string[];
+};
+
+export type Project = {
+	title: string;
+	image: string;
+	description: string;
+	link: string;
+};
+
+export type SocialNetwork = {
+	name: string;
+	link: string;
+};
