61091b350be3bf582c47340ff14e05c7b0f45763
Jorge Laptop <jlbmontejano@gmail.com>
2025-08-26 14:22:25 -0700

    Changed from React router to SPA structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { Route, Routes } from "react-router";
-import Menu from "./components/Menu";
-import AboutMe from "./sections/AboutMe";
-import Home from "./sections/Home";
-import Other from "./sections/Other";
-import Projects from "./sections/Projects";
-import WorkExperience from "./sections/WorkExperience";
+import Footer from './components/Footer';
+import Menu from './components/Menu';
+import AboutMe from './sections/AboutMe';
+import Home from './sections/Home';
+import Other from './sections/Other';
+import WorkExperience from './sections/WorkExperience';
 
 const App = () => {
 	return (
-		<div className='flex flex-col h-screen'>
+		<main>
+			<div className='absolute' id='home' />
 			<Menu />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about-me' element={<AboutMe />} />
-				<Route path='/work-experience' element={<WorkExperience />} />
-				<Route path='/projects' element={<Projects />} />
-				<Route path='/other' element={<Other />} />
-			</Routes>
-		</div>
+			<Home />
+			<AboutMe />
+			<WorkExperience />
+			<Other />
+			<Footer />
+		</main>
 	);
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
