0f68027e8d26cc71cf544f61b177ada13f160b58
Jorge Laptop <jlbmontejano@gmail.com>
2025-08-26 14:23:36 -0700

    Added page footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+		<div className='footer bg-charcoal text-off_white'>
+			<p>Created by Jorge Buenrostro</p>
+			<a
+				href='mailto:jlbmontejano@gmail.com'
+				target='_blank'
+				rel='noreferrer'>
+				Contact Me
+			</a>
+		</div>
+	);
+};
+
+export default Footer;
