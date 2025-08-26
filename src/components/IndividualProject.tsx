2777fe73d26817dd32fa72a3d6b2fd0471d29a97
Jorge Laptop <jlbmontejano@gmail.com>
2025-08-26 14:23:52 -0700

    Updated CSS

--- src/components/IndividualProject.tsx
+++ src/components/IndividualProject.tsx
@@ -1,22 +1,16 @@
-type IndividualProjectProps = {
-	project: {
-		title: string;
-		image: string;
-		description: string;
-		link: string;
-		code: string;
-	};
-};
+import { Project } from '../types';
 
-const IndividualProject = ({ project }: IndividualProjectProps) => {
+const IndividualProject = ({ project }: { project: Project }) => {
 	return (
-		<div className="flex flex-col gap-3">
-			<h4>{project.title}</h4>
+		<div className='flex flex-col gap-3'>
+			<p className='text-3xl font-semibold lg:text-5xl'>
+				{project.title}
+			</p>
 			<a href={project.link} target='_blank' rel='noreferrer'>
 				<img src={project.image} alt={`${project.title}-project`} />
 			</a>
 			<p className=''>{project.description}</p>
-			<a href={project.code} target='_blank' rel='noreferrer'>
+			<a href={project.link} target='_blank' rel='noreferrer'>
 				<button>CODE</button>
 			</a>
 		</div>
