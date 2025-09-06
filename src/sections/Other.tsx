0bfd9090025712be1ab7ec6ce5b7a6eeff7cbd52
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-07 11:24:26 +1200

    Fixed blank space bug by adding aos utility to individual elements

--- src/sections/Other.tsx
+++ src/sections/Other.tsx
@@ -6,22 +6,24 @@ const Other = () => {
 	return (
 		<div>
 			<div id='other' />
-			<div data-aos='fade-up' className='section-container'>
-				<p className='section-title'>── Other</p>
-				<div>
+			<div className='section-container'>
+				<p className='section-title' data-aos='fade-up'>
+					── Other
+				</p>
+				<div data-aos='fade-up'>
 					<p className='font-bold'>Languages:</p>
 					<ul className='list-none pl-0'>
 						<li>Spanish (Native)</li>
 						<li>English (Bilingual)</li>
 					</ul>
 				</div>
-				<div>
+				<div data-aos='fade-up'>
 					<p className='font-bold'>Education:</p>
 					<p>Bachelor's Degree in Music</p>
 					<p>Centro Universitario de Estudios Musicales</p>
 					<p>Queretaro, Mexico</p>
 				</div>
-				<div>
+				<div data-aos='fade-up'>
 					<p className='font-bold'>Location:</p>
 					<p>Auckland, New Zealand</p>
 				</div>
