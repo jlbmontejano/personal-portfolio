525cb4a09cf569c0b6fe459b803817bc571fc48e
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-07 11:23:44 +1200

    Added fade in effect

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,8 +9,10 @@ const Menu = () => {
 
 	return (
 		<div className='menu sticky top-0 z-50 bg-off_white sm:text-lg'>
-			<a onClick={() => handleClick("home")}>Jorge Buenrostro</a>
-			<div className='flex gap-3 lg:gap-6'>
+			<a onClick={() => handleClick("home")} className='animate-fadeIn'>
+				Jorge Buenrostro
+			</a>
+			<div className='flex animate-fadeIn gap-3 lg:gap-6'>
 				{MENU_OPTIONS.map(option => (
 					<a
 						onClick={() => handleClick(option.scrollTo)}
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,16 @@ export default {
 				accent: "#E3CAA6",
 			},
 		},
+		keyframes: {
+			fadeIn: {
+				"0%": { opacity: 0 },
+				"100%": { opacity: 1 },
+			},
+		},
+		animation: {
+			fadeIn: "fadeIn 1s ease-out forwards",
+		},
 	},
+
 	plugins: [],
 };
