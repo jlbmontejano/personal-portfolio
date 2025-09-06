1ddae5ee1ae8403fe52b1e2864247548a3644744
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-06 17:13:38 +1200

    Deleted unused fadeRight animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		fontFamily: {
-			sans: 'Inter',
+			sans: "Inter",
 		},
 		extend: {
 			colors: {
-				off_white: '#F8F9FA',
-				custom_gray: '#AEAEAE',
-				charcoal: '#212121',
-				accent: '#E3CAA6',
-			},
-			keyframes: {
-				fadeRight: {
-					'0%': { opacity: '0', transform: 'translateX(-10%)' },
-					'100%': { opacity: '1', transform: 'translateX(0)' },
-				},
-			},
-			animation: {
-				fadeRight: 'fadeRight 1s ease-out forwards',
+				off_white: "#F8F9FA",
+				custom_gray: "#AEAEAE",
+				charcoal: "#212121",
+				accent: "#E3CAA6",
 			},
 		},
 	},
