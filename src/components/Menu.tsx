90697f68e98f041152e279bba77c782e4d3f44c2
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-06 18:44:46 +1200

    Deleted AOS USAGE

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,6 @@
 import { MENU_OPTIONS } from "../constants/index";
-import useAOS from "../customHooks/useAos";
 
 const Menu = () => {
-	useAOS();
-
 	const handleClick = (scrollTo: string) => {
 		document
 			.getElementById(scrollTo)
@@ -12,10 +9,8 @@ const Menu = () => {
 
 	return (
 		<div className='menu sticky top-0 z-50 bg-off_white sm:text-lg'>
-			<a onClick={() => handleClick("home")} data-aos='fade-down'>
-				Jorge Buenrostro
-			</a>
-			<div className='flex gap-3 lg:gap-6' data-aos='fade-down'>
+			<a onClick={() => handleClick("home")}>Jorge Buenrostro</a>
+			<div className='flex gap-3 lg:gap-6'>
 				{MENU_OPTIONS.map(option => (
 					<a
 						onClick={() => handleClick(option.scrollTo)}
