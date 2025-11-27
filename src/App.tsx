a2e89a3eb84e90774cba7978d816c15879f9bffa
Jorge Laptop <jlbmontejano@gmail.com>
2025-11-27 13:53:55 +1300

    Added toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Footer from "./components/Footer";
-import Menu from "./components/Menu";
-import AboutMe from "./sections/AboutMe";
-import ContactForm from "./sections/ContactForm";
-import Experience from "./sections/Experience";
-import Home from "./sections/Home";
-import Other from "./sections/Other";
-import Projects from "./sections/Projects";
+import Footer from "@/components/Footer";
+import Menu from "@/components/Menu";
+import { Toaster } from "@/components/ui/sonner";
+import AboutMe from "@/sections/AboutMe";
+import ContactForm from "@/sections/ContactForm";
+import Experience from "@/sections/Experience";
+import Home from "@/sections/Home";
+import Other from "@/sections/Other";
+import Projects from "@/sections/Projects";
 
 const App = () => {
 	return (
@@ -16,9 +17,12 @@ const App = () => {
 			<AboutMe />
 			<Experience />
 			<Projects />
-			<Other />
-			<ContactForm />
+			<div className='flex flex-col sm:flex-row'>
+				<Other />
+				<ContactForm />
+			</div>
 			<Footer />
+			<Toaster />
 		</main>
 	);
 };
